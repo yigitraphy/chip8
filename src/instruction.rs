bf2805bd37006ec966702691de3d813160c7057c
Yiğit Gürbulak <78663763+el33tc0x4r@users.noreply.github.com>
2023-08-06 19:20:38 +0300

    Update instruction.rs

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,1 +1,25 @@
+pub struct Opcode(u16);
 
+impl Opcode {
+
+    fn oxoo(&self) -> usize {
+        ((self.0 & 0x0F00) >> 8) as usize 
+    }
+
+    fn ooyo(&self) -> usize {
+        ((self.0 & 0x00F0) >> 4) as usize 
+    }
+
+    fn ooon(&self) -> u8 {
+        (self.0 & 0x000F) -> as u8 
+    }
+
+    fn oonn(&self) -> u8 {
+        (self.0 & 0x00FF) -> as u8 
+    }
+
+    fn onnn(&self) -> u16 {
+        self.0 & 0x0FFF
+    }
+    
+} 
