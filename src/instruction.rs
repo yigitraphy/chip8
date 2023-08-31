5be487657d6ca667145e0ab7e0aeee63b38a3084
Yiğit Gürbulak <78663763+el33tc0x4r@users.noreply.github.com>
2023-08-31 07:45:55 +0300

    Update instruction.rs

--- src/instruction.rs
+++ src/instruction.rs
@@ -32,3 +32,43 @@ impl From<u16> for Opcode {
         Opcode(opcode)
     }
 }
+
+pub type Address = u16;
+pub type Register = usize;
+
+pub enum Instruction {
+    ClearDisplay,                                   
+    Return,                                         
+    Jump(Address),                                  
+    Call(Address),                                
+    SkipIfEqualsByte(Register, u8),                
+    SkipIfNotEqualsByte(Register, u8),            
+    SkipIfEqual(Register, Register),              
+    LoadByte(Register, u8),                        
+    AddByte(Register, u8),                        
+    Move(Register, Register),                      
+    Or(Register, Register),                   
+    And(Register, Register),                    
+    Xor(Register, Register),                     
+    Add(Register, Register),                   
+    Sub(Register, Register),
+    ShiftRight(Register),                     
+    ReverseSub(Register, Register),              
+    ShiftLeft(Register),                        
+    SkipIfNotEqualsByte(Register, Register),     
+    LoadI(Address),                             
+    JumpPlusZero(Address),                      
+    Random(Register, u8),                       
+    Draw(Register, Register, u8),               
+    SkipIfPressed(Register),                   
+    SkipIfNotPressed(Register),                
+    LoadDelayTimer(Register),                  
+    WaitForKeyPress(Register),                
+    SetDelayTimer(Register),                  
+    SetSoundTimer(Register),                  
+    AddToI(Register),                         
+    LoadSprite(Register),                    
+    BCDRepresentation(Register),             
+    StoreRegisters(Register),                
+    LoadRegisters(Register),                
+} 
