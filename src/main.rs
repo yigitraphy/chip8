e06d589d3ad4c2d5b13dae1de3f6ff10f2486dd0
Yiğit Gürbulak <78663763+el33tc0x4r@users.noreply.github.com>
2023-09-16 00:40:22 +0300

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::cast_possible_truncation)]
+#![warn(clippy::cast_lossless)]
+#![allow(clippy::unreadable_literal)]
+#![allow(clippy::non_ascii_literal)]
+
+mod display;
+mod emulator;
+mod instruction;
+mod keyboard;
+
+use emulator::Emulator;
+use std::env::args;
+
+fn main() {
+    Emulator::new()
+        .rom_oku(args().nth(1).unwrap_or_else(|| "brix.ch8".to_string()))
+        .expect("Error reading ROM")
+        .emulate();
+}
